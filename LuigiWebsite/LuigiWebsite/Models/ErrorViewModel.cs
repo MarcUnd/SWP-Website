55b879f2f17902a6be9c1eb7b0b571af93707dbe
MarcUnd <und.marc@gmail.com>
2021-11-04 10:31:41 +0100

    Anfang

--- LuigiWebsite/LuigiWebsite/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace LuigiWebsite.Models {
-    public class ErrorViewModel {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
