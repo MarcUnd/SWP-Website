aa91a3d7b301fb23f4ab712f928c2df4c2b53e71
MarcUnd <und.marc@gmail.com>
2021-11-12 19:15:05 +0100

    testtesttest

--- LuigiWebsite/LuigiWebsite/Controllers/HomeController.cs
+++ LuigiWebsite/LuigiWebsite/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace LuigiWebsite.Controllers {
+        //hello
     public class HomeController : Controller {
         public IActionResult Index() {
             return View();
