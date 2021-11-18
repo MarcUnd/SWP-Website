d0508618c6413cdbdf69e87e8138f0bdee22063c
MarcUnd <und.marc@gmail.com>
2021-11-18 11:29:45 +0100

    Menu

--- LuigiWebsite/LuigiWebsite/Controllers/MenuController.cs
+++ LuigiWebsite/LuigiWebsite/Controllers/MenuController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LuigiWebsite.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,23 @@ using System.Threading.Tasks;
 namespace LuigiWebsite.Controllers {
     public class MenuController : Controller {
         public IActionResult Menu() {
-            return View();
+            return View(MenuFromDB());
+        }
+
+        private List<MenuDB> MenuFromDB() {
+            return new List<MenuDB> {
+                new MenuDB{MenuId = 1, Preis = 7.90m, Name = "Pizza Margherita", Zutaten = "Tomaten, Mozarella, Basilikum"},
+                new MenuDB{MenuId = 2, Preis = 7.90m, Name = "Pizza Prosciuto", Zutaten = "Tomaten, Mozarella, Schinken"},
+                new MenuDB{MenuId = 3, Preis = 7.90m, Name = "Pizza Prosciuto E Funghi", Zutaten = "Tomaten, Mozarella, Schinken, Champignons"},
+                new MenuDB{MenuId = 4, Preis = 7.90m, Name = "Pizza Tonno", Zutaten = "Tomaten, Mozarella, Thunfisch, Zwiebeln"},
+                new MenuDB{MenuId = 5, Preis = 7.90m, Name = "Pizza Rustica", Zutaten = "Tomaten, Mozarella, Oliven, Zwiebeln"},
+                new MenuDB{MenuId = 6, Preis = 7.90m, Name = "Pizza Hawaii", Zutaten = "Tomaten, Mozarella, Schinken, Ananas"},
+                new MenuDB{MenuId = 7, Preis = 7.90m, Name = "Pizza Funghi", Zutaten = "Tomaten, Mozarella, Champignons"},
+                new MenuDB{MenuId = 8, Preis = 7.90m, Name = "Pizza Rucola", Zutaten = "Tomaten, Mozarella, Rucola, Parmesan, Knoblauch"},
+                new MenuDB{MenuId = 9, Preis = 7.90m, Name = "Pizza Vegetaria", Zutaten = "Tomaten, Mozarella, gegrilltes Gemüse, Champignons, Rucola, Knoblauch"},
+                new MenuDB{MenuId = 10, Preis = 7.90m, Name = "Pizza Calzone", Zutaten = "Tomaten, Mozarella, Schinken, Champignons, Salami"},
+                new MenuDB{MenuId = 11, Preis = 7.90m, Name = "Pizza Salami", Zutaten = "Tomaten, Mozarella, Salami"}
+            };
         }
     }
 }
---
+++ LuigiWebsite/LuigiWebsite/Models/MenuDB.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LuigiWebsite.Models {
+    public class MenuDB {
+
+        private int _menuId;
+        private decimal _preis;
+
+        public int MenuId {
+            get { return this._menuId; }
+            set { if (value >= 1) {
+                    this._menuId = value;
+                }
+            }
+        }
+
+        public decimal Preis {
+            get { return this._preis; }
+            set { if(value >= 1) {
+                    this._preis = value;
+                }
+            }
+        }
+
+        public String Name { get; set; }
+        public String Zutaten { get; set; }
+
+        
+    }
+}
