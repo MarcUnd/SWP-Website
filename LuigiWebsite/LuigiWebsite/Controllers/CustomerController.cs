bc9aa9beb91f5bfdfdfbb629b0517c698ff2222a
fabsifabs <fabian.lintner29@gmail.com>
2021-12-02 11:31:36 +0100

    validation

--- LuigiWebsite/LuigiWebsite/Controllers/CustomerController.cs
+++ LuigiWebsite/LuigiWebsite/Controllers/CustomerController.cs
@@ -11,12 +11,41 @@ namespace LuigiWebsite.Controllers {
             return View();
         }
         [HttpPost]
-        public IActionResult Resgistration(user userData) {
+        public IActionResult Registration(user userData) {
             if(userData == null) {
                 return RedirectToAction("Registration");
             }
-            return View();
+            ValidateRegistrationData(userData);
+            if (ModelState.IsValid) {
+                return RedirectToAction("Login");
+            }
+            return View(userData);
         }
+
+        private void ValidateRegistrationData(user u) {
+           if (u == null) {
+                return;
+            }
+           if(u.vorname == null) {
+                ModelState.AddModelError("vorname", "Bitte tragen sie einen Vornamen ein!");
+            }
+            if(u.nachname == null) {
+                ModelState.AddModelError("nachname", "Bitte tragen sie einen Nachnamen ein!");
+            }
+            if(u.password == null || u.password.Length < 8) {
+                ModelState.AddModelError("password", "Ihr Passwort muss mindesten acht Zeichen lang sein!");
+            }
+            if (u.BirthDate > DateTime.Now) {
+                ModelState.AddModelError("BirthDate", "Das Geburstdatum kann nicht in der Zukunft liegen!");
+            }
+            if(u.email == null) {
+                ModelState.AddModelError("email", "Bitte tragen sie eine Email-Addresse ein!");
+
+            }
+
+
+        }
+
         public IActionResult Login() {
             return View();
         }
