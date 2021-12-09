7fa523dc8d6d1dd9dfa815c1cb7357dfe89e3990
MarcUnd <und.marc@gmail.com>
2021-12-09 10:36:55 +0100

    reservation model

---
+++ LuigiWebsite/LuigiWebsite/Models/reservation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LuigiWebsite.Models {
+    public class reservation {
+
+        private int _resId;
+
+        public int ResId { 
+            get { return this._resId}
+            set { if (value >= 1) {
+                    this._resId = value;
+                }
+            } 
+        }
+
+        public string Nachname { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int Number { get; set; }
+    }
+}
