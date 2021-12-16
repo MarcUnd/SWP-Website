b3c9adb65023afcc48cd1eaeb8e672695caa2755
MarcUnd <und.marc@gmail.com>
2021-12-16 11:35:23 +0100

    menu

--- LuigiWebsite/LuigiWebsite/Controllers/MenuController.cs
+++ LuigiWebsite/LuigiWebsite/Controllers/MenuController.cs
@@ -13,17 +13,17 @@ namespace LuigiWebsite.Controllers {
 
         private List<MenuDB> MenuFromDB() {
             return new List<MenuDB> {
-                new MenuDB{MenuId = 1, Preis = 7.90m, Name = "Pizza Margherita", Zutaten = "Tomaten, Mozarella, Basilikum"},
-                new MenuDB{MenuId = 2, Preis = 7.90m, Name = "Pizza Prosciuto", Zutaten = "Tomaten, Mozarella, Schinken"},
-                new MenuDB{MenuId = 3, Preis = 7.90m, Name = "Pizza Prosciuto E Funghi", Zutaten = "Tomaten, Mozarella, Schinken, Champignons"},
-                new MenuDB{MenuId = 4, Preis = 7.90m, Name = "Pizza Tonno", Zutaten = "Tomaten, Mozarella, Thunfisch, Zwiebeln"},
-                new MenuDB{MenuId = 5, Preis = 7.90m, Name = "Pizza Rustica", Zutaten = "Tomaten, Mozarella, Oliven, Zwiebeln"},
-                new MenuDB{MenuId = 6, Preis = 7.90m, Name = "Pizza Hawaii", Zutaten = "Tomaten, Mozarella, Schinken, Ananas"},
-                new MenuDB{MenuId = 7, Preis = 7.90m, Name = "Pizza Funghi", Zutaten = "Tomaten, Mozarella, Champignons"},
-                new MenuDB{MenuId = 8, Preis = 7.90m, Name = "Pizza Rucola", Zutaten = "Tomaten, Mozarella, Rucola, Parmesan, Knoblauch"},
-                new MenuDB{MenuId = 9, Preis = 7.90m, Name = "Pizza Vegetaria", Zutaten = "Tomaten, Mozarella, gegrilltes Gemüse, Champignons, Rucola, Knoblauch"},
-                new MenuDB{MenuId = 10, Preis = 7.90m, Name = "Pizza Calzone", Zutaten = "Tomaten, Mozarella, Schinken, Champignons, Salami"},
-                new MenuDB{MenuId = 11, Preis = 7.90m, Name = "Pizza Salami", Zutaten = "Tomaten, Mozarella, Salami"}
+                new MenuDB{MenuId = 1, Preis = 6.90m, Name = "Pizza Margherita", Zutaten = "Tomaten, Mozarella, Basilikum"},
+                new MenuDB{MenuId = 2, Preis = 8.90m, Name = "Pizza Prosciuto", Zutaten = "Tomaten, Mozarella, Schinken"},
+                new MenuDB{MenuId = 3, Preis = 9.90m, Name = "Pizza Prosciuto E Funghi", Zutaten = "Tomaten, Mozarella, Schinken, Champignons"},
+                new MenuDB{MenuId = 4, Preis = 9.90m, Name = "Pizza Tonno", Zutaten = "Tomaten, Mozarella, Thunfisch, Zwiebeln"},
+                new MenuDB{MenuId = 5, Preis = 10.90m, Name = "Pizza Rustica", Zutaten = "Tomaten, Mozarella, Oliven, Zwiebeln"},
+                new MenuDB{MenuId = 6, Preis = 10.90m, Name = "Pizza Hawaii", Zutaten = "Tomaten, Mozarella, Schinken, Ananas"},
+                new MenuDB{MenuId = 7, Preis = 9.90m, Name = "Pizza Funghi", Zutaten = "Tomaten, Mozarella, Champignons"},
+                new MenuDB{MenuId = 8, Preis = 8.90m, Name = "Pizza Rucola", Zutaten = "Tomaten, Mozarella, Rucola, Parmesan, Knoblauch"},
+                new MenuDB{MenuId = 9, Preis = 10.90m, Name = "Pizza Vegetaria", Zutaten = "Tomaten, Mozarella, gegrilltes Gemüse, Champignons, Rucola, Knoblauch"},
+                new MenuDB{MenuId = 10, Preis = 11.90m, Name = "Pizza Calzone", Zutaten = "Tomaten, Mozarella, Schinken, Champignons, Salami"},
+                new MenuDB{MenuId = 11, Preis = 8.90m, Name = "Pizza Salami", Zutaten = "Tomaten, Mozarella, Salami"}
             };
         }
     }
